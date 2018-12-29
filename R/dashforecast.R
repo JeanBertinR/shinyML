d18aba800aa037260ecf5359ea54b3d6881531ff
JeanBertinR <jean.bertin@gadz.org>
2018-12-29 21:02:42 +0100

    Modification dash

--- R/dashforecast.R
+++ R/dashforecast.R
@@ -26,12 +26,12 @@ dashforecast <- function(data = data,y,date_column, share_app = FALSE,port = NUL
           
           box(
             title = "Controls",
-            sliderInput("slider", "Number of observations:",
+            sliderInput("range_selector", "Choose train and test period:",
                         eval(parse(text = paste0("min(data$",date_column,")"))),
                         eval(parse(text = paste0("max(data$",date_column,")"))),  
                         eval(parse(text = paste0("c(min(data$",date_column,"),max(data$",date_column,"-10),max(data$",date_column,"))"))))
-                        # as.Date("2015-01-01"), as.Date("2015-12-31"),
-                        # c(as.Date("2015-01-01"),as.Date("2015-06-01")))
+            # as.Date("2015-01-01"), as.Date("2015-12-31"),
+            # c(as.Date("2015-01-01"),as.Date("2015-06-01")))
           )
         )
       )
@@ -50,7 +50,11 @@ dashforecast <- function(data = data,y,date_column, share_app = FALSE,port = NUL
       output$input_curve <- renderDygraph({
         
         data <- as.data.table(data)
-        curve_entries <- dygraph(data = data) %>% dySeries(y,fillGraph = TRUE) 
+        curve_entries <- dygraph(data = data) %>%
+          dyShading(from = input$range_selector[1],to = input$range_selector[2],color = "lightyellow" ) %>% 
+          dyEvent(x = input$range_selector[1]) %>% 
+          dyEvent(x = input$range_selector[2]) %>% 
+          dySeries(y,fillGraph = TRUE) 
         
         
         # dyShading(from = data_index[cat_facturation == "sous facturation",]$Date[1], 
@@ -58,9 +62,9 @@ dashforecast <- function(data = data,y,date_column, share_app = FALSE,port = NUL
         #   #dyShading(from = "2017-1-1", to = "2018-1-1", color = "aliceblue") %>%
         #   dyEvent("2016-01-01", "année 2016", labelLoc = "bottom") %>% 
         #   dyEvent("2017-01-01", "année 2017", labelLoc = "bottom") %>% 
-          #eval(parse(text = paste0("dygraph(sequence_dates[,.(",date_column,",",y,")])")))
-
-          
+        #eval(parse(text = paste0("dygraph(sequence_dates[,.(",date_column,",",y,")])")))
+        
+        
         
         # if (lead(data$date_column,1)[1] - data$date_column[1] == 1){
         #   curve_entries <- curve_entries %>% dyBarChart()
@@ -91,13 +95,30 @@ dashforecast <- function(data = data,y,date_column, share_app = FALSE,port = NUL
 
 dashforecast(share_app = TRUE ,port = 7895,data =sequence_dates ,y = "Valeur",date_column = "Date")
 
+# Package h2o
+h2o.init(nthreads = -1)
+h2o_data <- as.h2o(iris)
+splits <- h2o.splitFrame(data = h2o_data,ratios = 0.75,seed = 1234)
+model <- h2o.gbm(x = 1:3,y = "Petal.Width",training_frame = splits[[1]])
+h2o.predict(model, splits[[2]])
 
 
+# Package sparklyr
+library(sparklyr)
+sc <- spark_connect(master = "local")
+data_conso_sparklyr <- copy_to(sc, iris, "iris", overwrite = TRUE)
+partitions <- sdf_partition(x =data_conso_sparklyr,training = 0.75,test = 0.25 )
 
 
+fit <- train %>%
+  ml_linear_regression(normal  ~ jour + mois ,   type = "regression")
 
 
+pred <- sdf_predict(fit, test) %>% collect %>% as.data.table()
+erreur_prev <- pred %>% summarise(mape = 100 * mean(abs((normal - prediction) / prediction)),
+                                  rmse = sqrt(mean((normal - prediction)**2)))
 
+iris
 
 sequence_dates <- seq.Date(from = as.Date("2017-01-01"),to = as.Date("2018-01-01"),by = "months") %>% 
   as.data.table() %>% 
