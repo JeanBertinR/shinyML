08c26e9f8a6b6b01f7d40695e36918d154294dc9
JeanBertinR <jean.bertin@gadz.org>
2019-01-31 15:32:29 +0100

    Ajout GLM

--- R/dashforecast.R
+++ R/dashforecast.R
@@ -36,7 +36,7 @@ sequence_dates <- sequence_dates %>%
 
 
 dashforecast <- function(data = data,x,y,date_column, share_app = FALSE,port = NULL ){
-  spark_disconnect(sc)
+  #spark_disconnect(sc)
   sc <- spark_connect(master = "local")
   
   
@@ -83,7 +83,7 @@ dashforecast <- function(data = data,x,y,date_column, share_app = FALSE,port = N
                         value = eval(parse(text = paste0("c(mean(data$",date_column,"),max(data$",date_column,"))")))),
             
             
-            actionButton("train_all","Train all models",style = 'color:white; background-color:red; padding:4px; font-size:150%',
+            actionButton("train_all","Run !",style = 'color:white; background-color:red; padding:4px; font-size:150%',
                          icon = icon("cogs",lib = "font-awesome"))
           ),
           
@@ -92,7 +92,7 @@ dashforecast <- function(data = data,x,y,date_column, share_app = FALSE,port = N
             
             
             sliderInput(label = "Step size",min = 0,max = 1, inputId = "step_size_gbm",value = 0.1),
-            sliderInput(label = "Subsampling rate",min = 0,max = 1, inputId = "subsampling_rate_gbm",value = 1),
+            sliderInput(label = "Subsampling rate",min = 0.1,max = 1, inputId = "subsampling_rate_gbm",value = 1),
             
             
             actionButton("run_gradient_boosting","Run gradient boosting model",style = 'color:white; background-color:darkgreen; padding:4px; font-size:150%',
@@ -105,18 +105,34 @@ dashforecast <- function(data = data,x,y,date_column, share_app = FALSE,port = N
             title = "Random Forest",
             
             sliderInput(label = "Number of trees",min = 1,max = 100, inputId = "num_tree_random_forest",value = 20),
-            sliderInput(label = "Subsampling rate",min = 0,max = 1, inputId = "subsampling_rate_random_forest",value = 1),
+            sliderInput(label = "Subsampling rate",min = 0.1,max = 1, inputId = "subsampling_rate_random_forest",value = 1),
             sliderInput(label = "Max depth",min = 0,max = 20, inputId = "max_depth_random_forest",value = 5),
             dataTableOutput("test_result"),
+            dataTableOutput("date_essai"),
             
-            # sliderInput(label = "Step size",min = 0,max = 1, inputId = "step_size_gbm",value = 0.1),
-            # sliderInput(label = "Subsampling rate",min = 0,max = 1, inputId = "subsampling_rate_gbm",value = 1),
             
-            actionButton("run_random_forest","Run random forest model model",style = 'color:white; background-color:darkblue; padding:4px; font-size:150%',
+            actionButton("run_random_forest","Run random forest model",style = 'color:white; background-color:darkblue; padding:4px; font-size:150%',
                          icon = icon("users",lib = "font-awesome"))
             # as.Date("2015-01-01"), as.Date("2015-12-31"),
             # c(as.Date("2015-01-01"),as.Date("2015-06-01")))
-          )  
+          ),
+          
+          box(
+            title = "Generalized linear regresion",
+            
+            # sliderInput(label = "Number of trees",min = 1,max = 100, inputId = "num_tree_random_forest",value = 20),
+            # sliderInput(label = "Subsampling rate",min = 0,max = 1, inputId = "subsampling_rate_random_forest",value = 1),
+            # sliderInput(label = "Max depth",min = 0,max = 20, inputId = "max_depth_random_forest",value = 5),
+            # dataTableOutput("test_result"),
+            # dataTableOutput("date_essai"),
+            
+            
+            actionButton("run_glm","Run generalized linear regression",style = 'color:white; background-color:orange; padding:4px; font-size:150%',
+                         icon = icon("cogs",lib = "font-awesome"))
+            # as.Date("2015-01-01"), as.Date("2015-12-31"),
+            # c(as.Date("2015-01-01"),as.Date("2015-06-01")))
+          )
+          
           
           
         )
@@ -135,6 +151,7 @@ dashforecast <- function(data = data,x,y,date_column, share_app = FALSE,port = N
       
       v_grad <- reactiveValues(type_model = NA)
       v_random <- reactiveValues(type_model = NA)
+      v_glm <- reactiveValues(type_model = NA)
       
       x <- reactiveValues(max_depth_random_forest = 5)
       
@@ -156,7 +173,15 @@ dashforecast <- function(data = data,x,y,date_column, share_app = FALSE,port = N
         
       })
       
-      
+      observeEvent(input$run_glm,{
+        #r$model <- "random_forest"
+        # t$num_tree_random_forest <- input$num_tree_random_forest
+        # v$subsampling_rate_random_forest <- input$subsampling_rate_random_forest
+        # x$max_depth_random_forest <-  input$max_depth_random_forest
+        
+        v_glm$type_model <- "ml_generalized_linear_regression"
+        
+      })
       
       
       
@@ -180,14 +205,30 @@ dashforecast <- function(data = data,x,y,date_column, share_app = FALSE,port = N
       })
       
       
+      test_1 <- reactiveValues(date = eval(parse(text = paste0("mean(data$",date_column,")"))))
+      
+      observeEvent(input$train_all,{
+        
+        test_1$date <- input$test_selector[1]
+        v_grad$type_model <- "ml_gradient_boosted_trees"
+        
+      })
+      
+      
+      output$date_essai <- renderDataTable({
+        test_1$date %>% as.data.table()
+      })
+      
+      
       
       table_forecast <- reactive({
         
         
         table_ml_gradient_boosted <- data.table(ml_gradient_boosted_trees = NA)
         table_ml_random_forest <- data.table(ml_random_forest = NA)
+        table_ml_glm <- data.table(ml_generalized_linear_regression = NA)
         
-        data_results = eval(parse(text = paste0("data[,.(",date_column,",",y,")][",date_column,">","'",input$test_selector[1],"',]")))
+        data_results = eval(parse(text = paste0("data[,.(",date_column,",",y,")][",date_column,">","'",test_1$date,"',]")))
         
         chaine_variable <- "mois"
         
@@ -196,8 +237,8 @@ dashforecast <- function(data = data,x,y,date_column, share_app = FALSE,port = N
         }
         
         
-        data_spark_train <- eval(parse(text = paste0( "data %>% filter(",date_column,"<= input$train_selector[2]) %>% as.data.table()")))
-        data_spark_test <- eval(parse(text = paste0("data %>% filter(",date_column,"> input$test_selector[1]) %>% as.data.table()")))
+        data_spark_train <- eval(parse(text = paste0( "data %>% filter(",date_column,"<='", test_1$date,"') %>% as.data.table()")))
+        data_spark_test <- eval(parse(text = paste0("data %>% filter(",date_column,">'", test_1$date,"') %>% as.data.table()")))
         
         data_spark_train <- copy_to(sc, data_spark_train, "data_spark_train", overwrite = TRUE)
         data_spark_test <- copy_to(sc, data_spark_test, "data_spark_test", overwrite = TRUE)
@@ -231,8 +272,21 @@ dashforecast <- function(data = data,x,y,date_column, share_app = FALSE,port = N
           
         }
         
+        if (!is.na(v_glm$type_model) & v_glm$type_model == "ml_generalized_linear_regression"){
+          
+          eval(parse(text = paste0("fit <- data_spark_train %>%",v_glm$type_model,"(", y ," ~ " ,chaine_variable ,
+                                   # ",num_trees  =",t$num_tree_random_forest,
+                                   # ",subsampling_rate =",v$subsampling_rate_random_forest,
+                                   # ",max_depth  =",x$max_depth_random_forest,
+                                   ")")))
+          
+          table_ml_glm <- sdf_predict(data_spark_test, fit) %>% collect %>% as.data.frame() %>% select(prediction)
+          names(table_ml_glm)[names(table_ml_glm) == 'prediction'] <- v_glm$type_model
+          
+        }
         
-        cbind(data_results,table_ml_gradient_boosted,table_ml_random_forest)
+        
+        cbind(data_results,table_ml_gradient_boosted,table_ml_random_forest,table_ml_glm)
         
       })
       
@@ -263,6 +317,11 @@ dashforecast <- function(data = data,x,y,date_column, share_app = FALSE,port = N
       observeEvent(input$train_selector,{
         updateSliderInput(session,'test_selector',
                           value= c(input$train_selector[2],input$test_selector[2]) ) 
+      })
+      
+      observeEvent(input$test_selector,{
+        updateSliderInput(session,'train_selector',
+                          value= c(input$train_selector[1],input$test_selector[1]) ) 
       })
       
       
@@ -373,7 +432,11 @@ data_spark_test <- copy_to(sc, data_spark_test, "data_spark_test", overwrite = T
 
 
 
-fit <- data_spark_train %>% ml_random_forest(Valeur ~ numero_jour,ladyers = c(1,2))
+fit <- data_spark_train %>% ml_generalized_linear_regression(Valeur ~ jour + numero_jour)
+fit <- data_spark_train %>% ml_logistic_regression(Valeur ~ jour + numero_jour)
+
+
+
 sdf_predict(data_spark_test, fit) %>% collect %>% as.data.frame()
 class(data_spark_train)
 
