a3e8f7c7573c214269795457b11afd53dd37cda8
JeanBertinR <jean.bertin@gadz.org>
2019-02-27 17:32:27 +0100

    Modification dash_h2o

--- R/dash_h2o.R
+++ R/dash_h2o.R
@@ -1,4 +1,4 @@
-#' @title Create a shiny app to implement and compare supervised regression models on time series (framework used: h2o)
+#' @title Create a shiny app to implement and compare supervised regression models on time series (framework used: Spark)
 #'
 #' @description This function creates in one line of code a shareable web app to compare supervised regression model performances.
 #'
@@ -18,7 +18,7 @@
 #'
 #' @examples
 #' longley2 <- longley %>% mutate(Year = as.Date(as.character(Year),format = "%Y"))
-#' dash_h20(data =longley2,x = c("GNP_deflator","Unemployed" ,"Armed_Forces", "Population","Employed"),y = "GNP",date_column = "Year",share_app = TRUE,port = 5845)
+#' dashforecast(data =longley2,x = c("GNP_deflator","Unemployed" ,"Armed_Forces", "Population","Employed"),y = "GNP",date_column = "Year",share_app = TRUE,port = 5845)
 #' @export
 
 dash_h20 <- function(data = data,x,y,date_column, share_app = FALSE,port = NULL ){
@@ -45,7 +45,12 @@ dash_h20 <- function(data = data,x,y,date_column, share_app = FALSE,port = NULL 
                  fluidRow(
                    fluidRow(
                      box(
-                       dygraphOutput("input_curve", height = 120, width = 930)),
+                       dygraphOutput("input_curve", height = 120, width = 930),
+                       dygraphOutput("output_curve", height = 120, width = 930)
+                     ),
+                     
+                     
+                     
                      box(
                        title = "Controls",
                        selectInput( inputId  = "input_variables",label = "Input variables: ",choices = x,multiple = TRUE,selected = x),
@@ -65,11 +70,22 @@ dash_h20 <- function(data = data,x,y,date_column, share_app = FALSE,port = NULL 
                        title = "Neural network model",status = "warning",
                        
                        materialSwitch(label = "Intercept term",inputId = "intercept_term_glm",status = "primary",value = TRUE),
-                       sliderInput(label = "Regularization parameter (lambda)",inputId = "reg_param_glm",min = 0,max = 10,value = 0),
-                       sliderInput(label = "Maximum iteraions",inputId = "max_iter_glm",min = 50,max = 300,value = 100),
-                       actionButton("run_neural_network","Run generalized linear regression",style = 'color:white; background-color:orange; padding:4px; font-size:150%',
+                       # sliderInput(label = "Regularization parameter (lambda)",inputId = "reg_param_glm",min = 0,max = 10,value = 0),
+                       # sliderInput(label = "Maximum iteraions",inputId = "max_iter_glm",min = 50,max = 300,value = 100),
+                       actionButton("run_neural_network","Run neural network regression",style = 'color:white; background-color:orange; padding:4px; font-size:150%',
                                     icon = icon("cogs",lib = "font-awesome"))
                        ,width = 3 ),
+                     
+                     box(
+                       title = "Decision tree",status = "danger",
+                       
+                       sliderInput(label = "Max depth",inputId = "max_depth_decision_tree",min = 0,max = 20,value = 5),
+                       sliderInput(label = "Max bins",inputId = "max_bins_decision_tree",min = 2,max = 60,value = 32),
+                       sliderInput(label = "Min instance per node",inputId = "min_instance_decision_tree",min = 1,max = 10,value = 1),
+                       actionButton("run_decision_tree","Run decision tree regression",style = 'color:white; background-color:red; padding:4px; font-size:150%',
+                                    icon = icon("cogs",lib = "font-awesome"))
+                       
+                       ,width = 3),
                      
                      dataTableOutput("table_test")
                      
@@ -85,14 +101,17 @@ dash_h20 <- function(data = data,x,y,date_column, share_app = FALSE,port = NULL 
     
     server = function(session, input, output) {
       set.seed(122)
-      model <- reactiveValues(train_variables = NA)
+      model <- reactiveValues()
       test_1 <- reactiveValues(date = eval(parse(text = paste0("mean(data$",date_column,")"))))
+      table_neural_network <- data.table(`Neural network` = NA)
+      v_neural <- reactiveValues(type_model = NA)
       
       
       observeEvent(input$run_neural_network,{
         
         test_1$date <- input$test_selector[1]
         model$train_variables <- input$input_variables
+        v_neural$type_model <- "ml_neural_network"
       })
       
       
@@ -119,40 +138,86 @@ dash_h20 <- function(data = data,x,y,date_column, share_app = FALSE,port = NULL 
       })
       
       
+      
+      output$output_curve <- renderDygraph({
+        
+        
+        output_dygraph <- dygraph(data = table_forecast()) %>% 
+          dyAxis("y",valueRange = c(0,1.5 * max(eval(parse(text =paste0("table_forecast()$",y)))))) 
+        
+        if (input$bar_chart_mode == TRUE){
+          output_dygraph <- output_dygraph %>% dyBarChart()
+        }
+        
+        output_dygraph %>% dyLegend(width = 800)
+        
+        
+        
+      })
+      
+      
+      
+      
+      
+      
+      
       table_forecast <- reactive({
         
-        data_results <- eval(parse(text = paste0("data.table:::`[.data.table`(data,j =.(",date_column,",",y,"))")))
-        var_input_list <- vector()
+        data_results <- eval(parse(text = paste0("data[,.(",date_column,",",y,")][",date_column,">'",test_1$date,"',]")))
+        var_input_list <- c()
         
         for (i in 1:length(model$train_variables)){
           var_input_list <- c(var_input_list,model$train_variables[i])
           
         }
         
-        #var_input_list <- ifelse(startsWith(var_input_list,"+"),substr(var_input_list,2,nchar(var_input_list)),var_input_list)
-        
-        
-        if (var_input_list != "+"){  
-          data_h2o_train <- eval(parse(text = paste0( "as.h2o(data %>% filter(",date_column,"<='", test_1$date,"') %>% as.data.table())")))
-          data_h2o_test <- eval(parse(text = paste0("as.h2o(data %>% filter(",date_column,">'", test_1$date,"') %>% as.data.table())")))
+        if (length(var_input_list) != 0){   
           
+          data_train <- eval(parse(text = paste0("data %>% filter(",date_column,"<='", test_1$date,"') %>% as.data.table()")))
+          data_test <- eval(parse(text = paste0("data %>% filter(",date_column,">'", test_1$date,"') %>% as.data.table()")))
           
-          dl_fit1 <- h2o.deeplearning(x = "Unemployed",
-                                      y = y,
-                                      training_frame = data_h2o_train,
-                                      model_id = "dl_fit1",
-                                      hidden = c(5,5),
-                                      seed = 1)
+          data_h2o_train <- as.h2o(data_train)
+          data_h2o_test <- as.h2o(data_test)
+          
+          if (!is.na(v_neural$type_model) & v_neural$type_model == "ml_neural_network"){
+            
+            dl_fit1 <- h2o.deeplearning(x = "Unemployed",
+                                        y = y,
+                                        training_frame = data_h2o_train,
+                                        model_id = "dl_fit1",
+                                        hidden = c(5,5),
+                                        seed = 1)
+            
+            table_neural_network <- h2o.predict(dl_fit1,data_h2o_test) %>% 
+              as.data.table() 
+            
+          }
           
         }
         
         
-        var_input_list %>% as.data.table()
+        
+        cbind(data_results,table_neural_network)%>% as.data.table()
+        
+        
+        
+        #length(var_input_list) %>% as.data.table()
+        #var_input_list %>% as.data.table()
         
       })
       
       output$table_test <-renderDataTable({
         table_forecast()
+      })
+      
+      observeEvent(input$train_selector,{
+        updateSliderInput(session,'test_selector',
+                          value= c(input$train_selector[2],input$test_selector[2]) ) 
+      })
+      
+      observeEvent(input$test_selector,{
+        updateSliderInput(session,'train_selector',
+                          value= c(input$train_selector[1],input$test_selector[1]) ) 
       })
       
     }
@@ -175,6 +240,7 @@ dash_h20 <- function(data = data,x,y,date_column, share_app = FALSE,port = NULL 
   
 }
 
+dash_h20(data =longley2,x = c("Unemployed","Armed_Forces"),y = "GNP",date_column = "Year",share_app = TRUE,port = 5845)
 
 
 dash_h20(data =longley2,x = c("GNP_deflator","Unemployed" ,"Armed_Forces", "Population","Employed"),y = "GNP",date_column = "Year",share_app = TRUE,port = 5845)
