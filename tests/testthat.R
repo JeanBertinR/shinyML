b050f761ee9c9f2f3f75ffc654481271f50ba8bb
JeanBertinR <jean.bertin@gadz.org>
2019-07-04 12:50:27 +0200

    Add to vignette !

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(shinyML)
-
-test_check("shinyML")
--- tests/testthat/test-shiny_h2o.R
+++
@@ -1,9 +0,0 @@
-context("shiny_h2o")
-
-
-
-test_that("shiny_h2o is an appropriate function", {
-  expect_that(shiny_h2o, is.function)
-
-  })
-
